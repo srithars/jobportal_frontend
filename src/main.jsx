379031345c460cc95b9d060f71720d02001f54da
drkpn <drkpn2019@gmail.com>
2025-07-07 20:27:44 +0530

    v7

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Dashboard from './Dashboard.jsx'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter base="/jobfrontapp1">
+  <BrowserRouter basename="/jobfrontapp1">
     <Routes>
-      <Route path='/' element={<App/>} />
-      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path="/" element={<App />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   </BrowserRouter>
 )
